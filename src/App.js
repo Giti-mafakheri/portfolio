faac9ecf87d81a64600fe46a73689f2b804130f6
giti-mafa <your@email.here.com>
2020-12-21 21:04:09 +1100

    1

--- src/App.js
+++ src/App.js
@@ -3,21 +3,36 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>About Me</h1>
-      <h3>
-        I am a junior web developer who has a passion for building user-friendly
-        and interactive web applications. I have graduated with an IT Bachelor’s
-        degree and worked for a number of years in the software development
-        industry. With my background in software help desk I have gained
-        experience working in at a team environment and interfacing with key
-        stack holders and clients . I have brushed up my skills trough studying
-        software engineering at General Assembly. I am looking for and
-        opportunity as a junior web developer to progress a career within IT
-        industry.
-      </h3>
-      <h2>Projects</h2>
-      <h1>Tic Tac Toe</h1>
-      <img src="./img/tic.png" alt="Tic"></img>
+      <div className="container">
+        <div className="header">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Contacts</li>
+          </ul>
+        </div>
+        <h3>
+          I am a junior web developer who has a passion for building
+          user-friendly and interactive web applications. I have graduated with
+          an IT Bachelor’s degree and worked for a number of years in the
+          software development industry. With my background in software help
+          desk I have gained experience working in at a team environment and
+          interfacing with key stack holders and clients . I have brushed up my
+          skills trough studying software engineering at General Assembly. I am
+          looking for and opportunity as a junior web developer to progress a
+          career within IT industry.
+        </h3>
+        <h2>Projects</h2>
+        <h1>Tic Tac Toe</h1>
+        <img src="./img/tic.png" alt="Tic"></img>
+        <div className="footer">
+          <ul>
+            <li>Likedin</li>
+            <li>GitHub</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
